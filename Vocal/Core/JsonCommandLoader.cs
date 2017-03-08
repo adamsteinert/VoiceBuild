fe99bbd846a428da44f0a92ba6d4430a7f7811ff
Adam Steinert <alphajulietsierra@gmail.com>
2017-03-07 22:37:07 -0600

    Move command file to appdata

--- Vocal/Core/JsonCommandLoader.cs
+++ Vocal/Core/JsonCommandLoader.cs
@@ -2,31 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Vocal.Model;
 
 namespace Vocal.Core
 {
     public class JsonCommandLoader
     {
+        const string AppDataFolder = "VocalBuildEngine";
         const string DefaultConfigFile = "commandsTemplate.json";
-        const string WriteableConfigFile = "commands.json";
+        const string WriteableCommandFile = "commands.json";
 
         public static IEnumerable<IVoiceCommand> LoadCommandsFromConfiguration()
         {
-            if(!File.Exists(WriteableConfigFile))
+            var commandFile = GetDefaultPath(WriteableCommandFile);
+
+            if (!File.Exists(commandFile))
             {
-                CreateWriteableConfigFile(DefaultConfigFile, WriteableConfigFile);
+                var file = new FileInfo(commandFile);
+
+                if (!Directory.Exists(file.Directory.FullName))
+                    Directory.CreateDirectory(file.Directory.FullName);
+
+                CreateWriteableConfigFile(DefaultConfigFile, commandFile);
             }
 
-            if (!File.Exists(WriteableConfigFile))
-                throw new Exception($"There was an error creating the command configuration file {WriteableConfigFile}. You may need to create this file manually.");
+            if (!File.Exists(commandFile))
+                throw new Exception($"There was an error creating the command configuration file {commandFile}. You may need to create this file manually.");
 
 
-            return JsonConvert.DeserializeObject<VoiceCommand[]>(File.ReadAllText(WriteableConfigFile));
+            return JsonConvert.DeserializeObject<VoiceCommand[]>(File.ReadAllText(commandFile));
         }
 
        
@@ -36,6 +40,11 @@ namespace Vocal.Core
                 throw new Exception($"The source configuration file {source} does not exist. A writeable configuration cannot be created.  See the documentation for more details.");
 
             File.Copy(source, destination);
+        }
+
+        public static string GetDefaultPath(string fileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppDataFolder, fileName);
         }
     }
 }
